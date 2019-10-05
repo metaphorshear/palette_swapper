28ae0c000b4a97ebabce1b591297c35a98b674b3
Marty Moradian <metaphorshearllc@gmail.com>
2019-10-05 08:24:42 -0500

    added some comments

--- release/javascripts/main.js
+++ release/javascripts/main.js
@@ -3,12 +3,15 @@
 var currentPalette;
 var skipFirstColor = true; //skip the first color in a palette, assuming it is a transparent color
 var labmemo = {};
+//the following line is uncommented in the server-side version
 //var worker = new Worker('worker.js');
 var loading;
 console.log(worker);
 //var deltae00memo = {}; //might add a selector so that users can try the other delta E formulas
 var nearestmemo = {};
 
+//this is a hack to let Web Workers run without needing a webserver
+//https://gist.github.com/walfie/a80c4432bcff70fb826d5d28158e9cc4
 function createWebWorkerFromFunction(f) {
   var blobContents = ['(', f.toString(), ')();'];
   var blob = new Blob(blobContents, { type: 'application/javascript'});
@@ -35,6 +38,7 @@ var workerUrl = buildPath('javascripts/worker.js');
 var includeUrl = buildPath('javascripts/deltae.global.min.js');
 
 worker.postMessage({ scriptUrls: [workerUrl, includeUrl] });
+//end client-side Web Worker hack
 
 
 function d3lab(r, g, b) {
@@ -50,15 +54,20 @@ function d3lab(r, g, b) {
 }
 
 function drawImageFromFile() {
+    //this is the original image
     let canvas = document.getElementById('image_before');
+    //the modified image appears here
     let canvasB = document.getElementById('image_after');
+    //normal Canvas setup
     let ctx = canvas.getContext('2d');
     let img = new Image();
+    //this is the file, chosen by the user in the file selector
     let curFile = document.getElementById("uploadImage").files[0];
     let url = window.URL || window.webkitURL;
     let src = url.createObjectURL(curFile);
     img.src = src;
     img.onload = function () {
+        //scale the images so they don't go off the screen
         let width = this.width;
         let height = this.height;
         if (this.height > window.screen.availHeight) {
@@ -75,6 +84,7 @@ function drawImageFromFile() {
         canvas.height = height;
         canvasB.width = width;
         canvasB.height = height;
+        //draw
         ctx.drawImage(img, 0, 0, width, height);
         url.revokeObjectURL(src);
         canvasB.getContext('2d').drawImage(canvas, 0, 0); //this copies the image to the right before processing happens.  it's not necessary, but I kind of like it.
@@ -83,17 +93,21 @@ function drawImageFromFile() {
 
 function drawPalette() {
     //this should be called by paletteSetup
+    //this sets up the canvas that displays the color swatches
     let canvas = document.getElementById('palette_display');
     let ctx = canvas.getContext('2d');
     let width = window.screen.availWidth * 0.75;
     canvas.width = width;
+    //if the palette is small enough, it can be displayed with nice big squares
     let swatchSize = Math.ceil(width / currentPalette.length);
     let rows = 1;
+    //20px is about the smallest I want one of the color-swatch squares to be
     if (swatchSize < 20) {
         swatchSize = 20;
         rows = (swatchSize * currentPalette.length) / width;
     }
     canvas.height = Math.ceil(rows) * swatchSize;
+    //the actual drawing step
     _.each(currentPalette, function (swatch, index) {
         ctx.fillStyle = swatch.toString();
         ctx.fillRect((index * swatchSize) % width, Math.floor((index * swatchSize) / width)*swatchSize, swatchSize, swatchSize);
@@ -102,9 +116,11 @@ function drawPalette() {
 }
 
 function paletteSetup() {
+    //this removes black, which is often included at the beginning of palettes for some reason
     if (skipFirstColor) {
         currentPalette = _.rest(currentPalette);
     }
+    //pre-calculate the Lab color for each (rgb) color in the palette
     for (var i = 0; i < currentPalette.length; ++i) {
         let swatch = d3.color(currentPalette[i].toString());
         currentPalette[i].d3color = swatch;
@@ -116,7 +132,9 @@ function paletteSetup() {
 }
 
 function loadPaletteFromFile() {
+    //this is pretty similar to drawImageFromFile, but with palettes
     let file = document.getElementById("uploadPalette").files[0];
+    //only the file extension is checked; i.e., it might be a valid file but need renaming
     let supportedByAnypalette = ['pal', 'gpl', 'txt', 'psppalette', 'hpl', 'cs', 'wpe'];
     if (file.name.endsWith('ase')) {
         alert("No ASE support yet, sorry.");
@@ -130,6 +148,7 @@ function loadPaletteFromFile() {
             else if (error) {
                 alert(error);
             }
+                //this would mean that AnyPalette didn't catch an error, but also didn't return a palette
             else {
                 alert("Something has gone horribly wrong.");
             }
@@ -141,27 +160,32 @@ function loadPaletteFromFile() {
 }
 
 function findNearestColor(pixel, palette) {
-    //pixel: an array with three components
+    //pixel: an array with three components (representing the colors of a pixel)
     //palette: an array of objects
     //this assumes the anypalette format, where each object contains properties for r, g, and b
     //and also a toString method which returns a CSS Color Module Level 3 specifier string
     //e.g., "rgb(255, 255, 255)"
+
     let key = pixel[0] << 16 | pixel[1] << 8 | pixel[2];
+    //this function is memoized, and its results are cached in nearestmemo
     if (nearestmemo[key]) {
         return nearestmemo[key];
     }
     let deltaEs = palette.map(function (color) {
-        //color is the object
+        //color is a palette swatch object
         let swatch = color.d3color;
         let labSwatch = color.lab;
         let labPixel = d3lab(pixel[0], pixel[1], pixel[2]);
         let obj = {
             color: swatch,
+            //this is the part of the code that takes the longest
+            //it finds the distance between the color of this pixel and the color of the current palette swatch
             deltaE: DeltaE.getDeltaE00(
                 { L: labSwatch.l, A: labSwatch.a, B: labSwatch.b }, { L: labPixel.l, A: labPixel.a, B: labPixel.b })
         };
         return obj;
     });
+    //this part actually finds the nearest color, by finding the minimum distance
     let result = _.min(deltaEs, _.iteratee('deltaE'));
     nearestmemo[key] = result;
     return result;
@@ -170,16 +194,22 @@ function findNearestColor(pixel, palette) {
 function swapColors() {
     let canvasA = document.getElementById('image_before');
     let canvasB = document.getElementById('image_after');
+    
     let ctx = canvasB.getContext('2d');
     //ctx.drawImage(canvasA, 0, 0);  //not necessary, but a matter of taste.  displays the original image on both sides before processing is done
     let imageData = ctx.getImageData(0, 0, canvasA.width, canvasA.height);
     
     if (window.Worker) {
+        //this is running in its own thread and won't hang the page (yay!)
         worker.postMessage([imageData.data, currentPalette]);
         console.log("message posted to worker");
+
+        //I was experimenting with a loader image, but it hasn't worked out so far
         //document.getElementsByClassName('loader')[0].setAttribute("style", `display: block; position: absolute; left: 70%; top: 60%`)
     }
     else {
+        //it sucks if we get down here.  bigger image/palette combinations will take a while, and the browser will complain.
+        //this is mostly the same code as in the worker.  I could not figure out a solution besides copying it.
         let data = new Uint8ClampedArray(imageData.data);
         data.set(imageData.data);
         let t0 = performance.now();
@@ -190,7 +220,7 @@ function swapColors() {
             data[i + 2] = color.b;
         }
         let t1 = performance.now();
-        console.log(`Time to swap colors of whole image: ${t1 - t0} ms`);
+        console.log(`Time to swap colors of whole image: ${t1 - t0} ms`); //probably a lot
         ctx = canvasB.getContext('2d');
         let imageDataB = ctx.getImageData(0, 0, canvasB.width, canvasB.height);
         imageDataB.data.set(data);
@@ -204,6 +234,7 @@ worker.onmessage = function (e) {
     let imageData = ctx.getImageData(0, 0, canvasB.width, canvasB.height);
     imageData.data.set(e.data[0]);
     ctx.putImageData(imageData, 0, 0);
+     //since the Web Worker doesn't have access to nearestmemo, its copied back and forth between the threads
     nearestmemo = e.data[1];
     //document.getElementsByClassName('loader')[0].setAttribute("style", "display: none");
     console.log("message received from worker");
